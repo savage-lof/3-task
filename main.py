1fa0d8a1c34b3785ce11e447956ca6197a109a9f
savage-lof <lomova20042004@ya.ru>
2020-11-21 00:58:27 +0300

    dont see button

--- main.py
+++ main.py
@@ -1,59 +1,31 @@
 import random
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QPainter, QColor
-from ui_file import Ui_MainWindow
+from untitled import Ui_Form
 
 
-
-class WindowDraw(QWidget):
+class WindowDraw(QWidget, Ui_Form):
     def init(self):
         super().init()
         self.setupUi(self)
-        self.setMouseTracking(True)
-        self.coord = []
-        self.attr = None
-
+        self.pushButton.clicked.connect(self.run)
 
     def paintEvent(self, event):
         self.qp = QPainter()
         self.qp.begin(self)
-        self.draw(self.attr)
         self.qp.end()
 
-
-    def draw(self, attr):
-        const = random.randint(10, 200)
-        mass = [i for i in range(0, 256)]
-        self.qp.setBrush(QColor(random.choice(mass), random.choice(mass), random.choice(mass)))
-        if attr == 'left':
-            self.qp.drawEllipse(*self.coord, const, const)
-        elif attr == 'right':
-            self.qp.drawRect(*self.coord, const, const)
-        elif attr == 'space':
-            self.qp.drawPolygon(QPoint(self.coord[0] - (const * 1.3), self.coord[1]),
-                                QPoint(self.coord[0] + const, self.coord[1] + const),
-                                QPoint(self.coord[0] + const, self.coord[1] - const))
-
-    def mouseMoveEvent(self, event):
-        self.coord = [event.x(), event.y()]
-
-    def mousePressEvent(self, event):
-        self.coord = [event.x(), event.y()]
-        if event.button() == Qt.LeftButton:
-            self.attr = 'left'
-        elif event.button() == Qt.RightButton:
-            self.attr = 'right'
-        self.update()
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self.attr = 'space'
-        self.update()
+    def run(self):
+        self.qp.setBrush(QColor('#f7ff00'))
+        i = random.randint(0, 500)
+        j = random.randint(0, 500)
+        const = random.randint(10, 60)
+        self.qp.drawEllipse(i, j, const, const)
 
 
-if name == 'main':
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = WindowDraw()
     ex.show()
